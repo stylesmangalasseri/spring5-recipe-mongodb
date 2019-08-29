e2db0bb5c23c9afa0ec94b4ba114055b50a2c545
Styles.Mangalasseri <stylesmji@gmail.com>
2019-08-29 14:38:00 +0200

    initial commit

---
+++ src/main/java/com/example/recipeapp/Spring5RecipeMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RecipeMongodbApplication.class, args);
+	}
+
+}
